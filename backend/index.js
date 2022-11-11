ef78677d67d345c16aae6b561da974f787d8c31d
Mathew Morse <mrm1779@gmail.com>
2022-11-10 23:27:03 -0500

    dotenv, twilio, and test sms sent

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const dotenv = require('dotenv').config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    client.messages
+        .create({
+            body: 'The cake is a lie',
+            from: '+13393304005',
+            to: '+13153828467'
+        }).then(message => {
+            res.send(message.sid);
+        })
 });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+//POST request: Number to text with a response back to the front-end on success
+
+//GET request?: Send information to the text api
+
+//Function for sending out texts
+
+//GET request?: Get a response back from the dickhead to cancel the texts
+
+//GET request?: Wait for text responses from the dickhead and post them to the front-end
